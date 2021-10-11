0fba86588589aba3deed93e44ffcd143bc71566b
Jean F Silva <mandakeru@gmail.com>
2021-10-11 17:55:50 -0300

    frontend finished, logic to handle search working

---
+++ app/javascript/components/Commons/RepoWidget/RepoWidget.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="card repo-widget">
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-48x48">
+          <img :src="repoParams.owner.avatar_url" alt="">
+        </figure>
+
+        <div class="media-content">
+          <div class="media-content">
+            <p class="title is-4">Owner: <a :href="repoParams.owner.url">{{ repoParams.owner.login }}</a></p>
+            <p class="subtitle is-6"><a :href="repoParams.owner.repos_url">Repos Url</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="content">
+        <p class="title is-4">{{ repoParams.name }}</p>
+        <p class="subtitle is-6">{{ repoParams.full_name }}</p>
+        <a :href="repoParams.languages_url">{{ repoParams.language }}</a>
+        <p class="subtitle is-6"><i class="fas fa-star"></i>{{ repoParams.stars }}</p>
+        <p class="subtitle is-6"><i class="fas fa-code-branch"></i>{{ repoParams.forks }}</p>
+        <br>
+        <time datetime="2016-1-1">Last Update: {{ repoParams.updated_at }}</time>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'RepoWidget',
+  props: {
+    repoParams: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+.repo-widget {
+  margin-bottom: 20px;
+}
+</style>
--- app/javascript/pages/Home.vue
+++ app/javascript/pages/Home.vue
@@ -1,24 +1,124 @@
 <template>
-  <form class="box">
-  <div class="field">
-    <label class="label">Type to seach in Github repos</label>
-    <div class="control">
-      <input class="input" type="email" v-model="term" placeholder="e.g. react.js">
+<div class="content-wrapper">
+  <div class="box">
+    <div class="field">
+      <label class="label">Type to seach in Github repos</label>
+      <div class="control">
+        <input class="input" type="search" v-model="term" placeholder="e.g. react.js">
+      </div>
     </div>
+
+    <div class="row">
+      <div class="field">
+        <label class="label">Sort options</label>
+        <div class="control">
+          <div class="select">
+            <select v-model="sort">
+              <option>Stars</option>
+              <option>Forks</option>
+              <option>Updated</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div class="field mx-5">
+        <label class="label">Programing Languages</label>
+        <div class="control">
+          <div class="select">
+            <select v-model="language">
+              <option>Ruby</option>
+              <option>Elixir</option>
+              <option>Php</option>
+              <option>Java</option>
+              <option>Javascript</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div class="field  mx-5">
+        <label class="label">Total per page</label>
+        <div class="control">
+          <div class="select">
+            <select v-model="perPage">
+              <option>5</option>
+              <option>10</option>
+              <option>20</option>
+              <option>30</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <button class="button is-primary" @click="submitSearch">search</button>
   </div>
 
-  <button class="button is-primary">search</button>
-</form>
+  <PulseLoader :loading="loading" color="green" size="20px"></PulseLoader>
+  <div class="repos-wrapper" v-if="repos.length > 1">
+    <div v-for="repo in repos" :key="repo.name + repo.stars">
+      <repo-widget :repoParams="repo" />
+    </div>
+
+  </div>
+</div>
 </template>
 
 <script>
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
+import axios from 'axios';
+import RepoWidget from '../components/Commons/RepoWidget/RepoWidget'
+import moment from 'moment'
+
 export default {
+  components: { RepoWidget, PulseLoader },
   name: 'Home',
 
   data() {
     return {
-      term: ''
+      term: 'react',
+      sort: 'Stars',
+      language: 'Ruby',
+      perPage: '5',
+      repos: [],
+      loading: false
+    }
+  },
+
+  methods: {
+    submitSearch() {
+      this.repos = []
+      this.loading = true
+      axios.get(`api/v1/search?term=${this.term}&language=${this.language}&sort=${this.sort}&per_page=${this.perPage}`)
+        .then(response => {
+          console.log(response)
+          let items = response.data.items
+          items.map(item => {
+            this.repos.push({
+              forks: item.forks,
+              full_name: item.full_name,
+              home_page: item.home_page,
+              language: item.language,
+              languages_url: item.languages_url,
+              name: item.name,
+              open_issues: item.open_issues,
+              owner: item.owner,
+              stars: item.stargazers_count,
+              visibility: item.visibility,
+              updated_at: moment(item.updated_at).format('MMM Do YY')
+
+            })
+          })
+
+          this.loading = false
+        })
     }
   }
 }
 </script>
+<style>
+.row {
+  display: flex;
+}
+</style>
